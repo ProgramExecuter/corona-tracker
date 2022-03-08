581a6b06765a94b9a41745933cc94b6816438f6b
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-08 22:12:32 +0530

    Basic Header Setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <img className="header__logo" src="./header-logo.png" />
+      <select className="header__select">
+        <option value="All">All</option>
+        <option value="India">India</option>
+      </select>
+    </header>
+  );
 };
 
 export default Header;
