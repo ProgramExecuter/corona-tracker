db1b29655b737265696bef92d8d6387993c8f7d5
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-09 14:06:48 +0530

    Created complete structure of 'DataBox' component

--- src/components/DataBox/DataBox.js
+++ src/components/DataBox/DataBox.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "./DataBox.css";
 
 const DataBox = () => {
-  return <div>DataBox</div>;
+  return (
+    <div className="databox">
+      <div className="databox__confirmed databox__child">
+        <span className="main_count">
+          {"133,245,113"} <br /> Cases
+        </span>
+        <span className="secondary_count">+ {"2,234"}</span>
+      </div>
+      <div className="databox__death databox__child">
+        <span className="main_count">
+          {"123,192"} <br /> Deaths
+        </span>
+        <span className="secondary_count">+ {"8"}</span>
+      </div>
+      <div className="databox__recovered databox__child">
+        <span className="main_count">
+          {"132,245,113"} <br /> Recovered
+        </span>
+        <span className="secondary_count">+ {"23,234"}</span>
+      </div>
+    </div>
+  );
 };
 
 export default DataBox;
