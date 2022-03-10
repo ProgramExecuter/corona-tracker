cf5a5bd9bc0ea501e302eff73e8a94727e1dd130
Amit Singh Rana <ranaa73000@gmail.com>
2022-03-10 14:39:49 +0530

    Added full API functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import Graph from "./components/Graph/Graph";
-import Footer from "./components/Footer/Footer";
-
+///
 // For sorting data
+///
 const sortData = (data) => {
   let sortedData = [...data];
   sortedData.sort((a, b) => {
@@ -19,53 +18,35 @@ const sortData = (data) => {
 
 const App = () => {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(["all"]);
+  const [graphData, setGraphData] = useState({});
 
   const [confirmed, setConfirmed] = useState({ total: "0", today: "0" });
   const [death, setDeath] = useState({ total: "0", today: "0" });
   const [recovered, setRecovered] = useState({ total: "0", today: "0" });
+  const [caseType, setCaseType] = useState("cases");
 
+  ///
   // Get all countries of the world
-  useEffect(() => {
-    const getCountries = async () => {
-      fetch("https://disease.sh/v3/covid-19/countries/")
-        .then((response) => response.json())
-        .then((data) => {
-          const countries = data.map((country) => ({
-            name: country.country,
-            value: country.countryInfo.iso3,
-          }));
+  ///
+  const getCountries = async () => {
+    fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          name: country.country,
+          value: country.countryInfo.iso3,
+        }));
 
-          let sortedData = sortData(countries);
-
-          setCountries(sortedData);
-        })
-        .catch((err) => console.log(err));
-    };
-
-    getCountries();
-  }, []);
-
-  // Get 'WORLDWIDE' data
-  useEffect(() => {
-    const url = "https://disease.sh/v3/covid-19/all";
-
-    getData(url);
-  }, []);
-
-  // Change country and get new data
-  const countryChange = (e) => {
-    const selectedCountry = e.target.value;
-
-    const url =
-      selectedCountry === "Worldwide"
-        ? "https://disease.sh/v3/covid-19/all"
-        : `https://disease.sh/v3/covid-19/countries/${selectedCountry}`;
-
-    getData(url);
+        let sortedData = sortData(countries);
+        setCountries(sortedData);
+      })
+      .catch((err) => console.log(err));
   };
 
-  //
-  // For fetching data
+  ///
+  // For fetching data for databoxes
+  ///
   const getData = async (url) => {
     fetch(url)
       .then((response) => response.json())
@@ -77,12 +58,71 @@ const App = () => {
       .catch((err) => console.log(err));
   };
 
+  ///
+  // Fetch historical data for graph
+  ///
+  const fetchHistoricalData = async (url, caseType) => {
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("graph", graphData);
+        setGraphData(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  ///
+  // On first page loading
+  ///
+  useEffect(() => {
+    // Get all countries in the world
+    getCountries();
+
+    // get data for databoxes
+    let url = "https://disease.sh/v3/covid-19/all";
+    getData(url);
+
+    // get historical data for graph
+    url = "https://disease.sh/v3/covid-19/historical/all";
+    fetchHistoricalData(url, caseType);
+  }, []);
+
+  ///
+  // Fetch historical data when changing caseType OR country
+  ///
+  useEffect(() => {
+    const url =
+      country == "all"
+        ? "https://disease.sh/v3/covid-19/historical/all"
+        : `https://disease.sh/v3/covid-19/historical/${country}`;
+
+    fetchHistoricalData(url, caseType);
+  }, [caseType, country]);
+
+  ///
+  // Fetch databox data when changing country
+  ///
+  useEffect(() => {
+    // get data for databoxes
+    let url =
+      country == "all"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${country}`;
+    getData(url);
+  }, [country]);
+
   return (
     <>
       <header className="header">
         <img alt="web logo" className="header__logo" src="./header-logo.png" />
-        <select className="header__select" onChange={countryChange}>
-          <option key="Worldwide" value="Worldwide">
+        <select
+          className="header__select"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        >
+          <option key="all" value="all">
             Worldwide
           </option>
           {countries.map((country, idx) => {
@@ -96,28 +136,40 @@ const App = () => {
       </header>
 
       <div className="databox">
-        <div className="databox__confirmed databox__child">
+        <div
+          className="databox__confirmed databox__child"
+          onClick={() => {
+            setCaseType("cases");
+          }}
+        >
           <span className="main_count">
             {confirmed.total} <br /> Cases
           </span>
           <span className="secondary_count">+ {confirmed.today}</span>
         </div>
-        <div className="databox__death databox__child">
+        <div
+          className="databox__death databox__child"
+          onClick={() => {
+            setCaseType("deaths");
+          }}
+        >
           <span className="main_count">
             {death.total} <br /> Deaths
           </span>
           <span className="secondary_count">+ {death.today}</span>
         </div>
-        <div className="databox__recovered databox__child">
+        <div
+          className="databox__recovered databox__child"
+          onClick={() => {
+            setCaseType("recovered");
+          }}
+        >
           <span className="main_count">
             {recovered.total} <br /> Recovered
           </span>
           <span className="secondary_count">+ {recovered.today}</span>
         </div>
       </div>
-
-      <Graph />
-      <Footer />
     </>
   );
 };
